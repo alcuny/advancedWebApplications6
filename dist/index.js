5fdbcf5fe52769169278a8ace343238f33a9408f
Albert Cuny01~ <cunyalbert16@gmail.com01~>
2023-12-10 19:29:56 +0200

    final commit ex4

--- dist/index.js
+++ dist/index.js
@@ -10,26 +10,29 @@ const port = 3000;
 // Use body-parser middleware to parse JSON bodies
 app.use(express_1.default.json());
 app.get("/hello", (req, res) => {
-    res.send("Hello world!####");
+    res.send("Hello world!");
 });
 const vehiclesList = [];
+// // post
 app.post("/vehicle/add", (req, res) => {
-    //console.log(req.body);
     let newVehicle = {
         model: req.body.model,
         color: req.body.color,
         year: req.body.year,
         power: req.body.power
     };
+    // car
     if (req.body.bodyType) {
         //console.log("hei")
         newVehicle.bodyType = req.body.bodyType;
         newVehicle.wheelCount = req.body.wheelCount;
     }
+    // boat
     if (req.body.draft) {
         //console.log("hei")
         newVehicle.draft = req.body.draft;
     }
+    // plane
     if (req.body.wingspan) {
         //console.log("hei")
         newVehicle.wingspan = req.body.wingspan;
@@ -37,6 +40,22 @@ app.post("/vehicle/add", (req, res) => {
     vehiclesList.push(newVehicle);
     console.log(newVehicle);
     res.status(201).send("Vehicle added");
+});
+app.get("/vehicle/search/:model", (req, res) => {
+    let searchModel = req.params.model;
+    // assigning the value to undefined first
+    let SearchVehicle = undefined;
+    vehiclesList.forEach(vehicle => {
+        if (vehicle.model === searchModel) {
+            SearchVehicle = vehicle;
+        }
+    });
+    if (SearchVehicle) {
+        res.send(SearchVehicle);
+    }
+    else {
+        res.status(404).send("Vehicle not found");
+    }
 });
 app.listen(port, () => {
     console.log("server is running at http://localhost:" + port);
--- index.ts
+++ index.ts
@@ -8,11 +8,13 @@ const port: number = 3000;
 app.use(express.json());
 
 app.get("/hello", (req: Request, res: Response) => {
-    res.send("Hello world!####");
+    res.send("Hello world!");
 })
 
-// post
 
+
+
+// 
 interface Vehicle {
     model: string;
     color: string;
@@ -25,29 +27,28 @@ interface Vehicle {
   }
 
 const vehiclesList: Vehicle[] = [];  
-
+// // post
 app.post("/vehicle/add", (req: Request, res: Response) => {
-    //console.log(req.body);
    
-
     let newVehicle: Vehicle = {
         model: req.body.model,
         color: req.body.color,
         year: req.body.year,
         power: req.body.power
     }
+    // car
     if(req.body.bodyType){
         //console.log("hei")
         newVehicle.bodyType = req.body.bodyType;
         newVehicle.wheelCount = req.body.wheelCount;
     }
-
+    // boat
     if(req.body.draft){
         //console.log("hei")
         newVehicle.draft = req.body.draft;
         
     }
-
+    // plane
     if(req.body.wingspan){
         //console.log("hei")
         newVehicle.wingspan = req.body.wingspan;
@@ -58,6 +59,34 @@ app.post("/vehicle/add", (req: Request, res: Response) => {
     console.log(newVehicle);
 
     res.status(201).send("Vehicle added");
+})
+// searching Vehiclelist by model type
+app.get("/vehicle/search/:model", (req: Request, res: Response) => {
+    
+    let searchModel: string = req.params.model;
+    // assigning the value to undefined first
+    let SearchVehicle: Vehicle | undefined = undefined;
+
+    vehiclesList.forEach(vehicle => {
+
+        if(vehicle.model === searchModel){
+
+            SearchVehicle = vehicle; 
+
+        }
+
+    })
+
+    if(SearchVehicle){
+
+        res.send(SearchVehicle);
+
+    }else{
+
+        res.status(404).send("Vehicle not found");
+
+    }
+
 })
 
 app.listen(port, () => {
